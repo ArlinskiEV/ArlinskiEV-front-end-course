e93f75b9adf82cc8ba5132d424d41a8f6eccb9a8
ArlinskiEV <ArlinskiEV@gmail.com>
2017-11-03 01:09:33 +0300

    match-match-game/feature: Game as CS6 class, listener on timer&&click

--- match-match-game/js/scrypt.js
+++ match-match-game/js/scrypt.js
@@ -1,47 +1,104 @@
- function Game() {
-     alert('hello world ;)');
+//implement shuffle-function
+Array.prototype.shuffle = function() {
+    let input = this;
+
+    for (let i = input.length - 1; i >= 0; i--) {
+
+        let randomIndex = Math.floor(Math.random() * (i + 1));
+
+        let itemAtIndex = input[randomIndex];
+        input[randomIndex] = input[i];
+        input[i] = itemAtIndex;
+    }
+    return input;
+}
 
 
-
-    Array.prototype.shuffle = function() {
-        let input = this;
-
-        for (let i = input.length - 1; i >= 0; i--) {
-
-            let randomIndex = Math.floor(Math.random() * (i + 1));
-
-            let itemAtIndex = input[randomIndex];
-            input[randomIndex] = input[i];
-            input[i] = itemAtIndex;
+class Game {
+    constructor() {
+        let gamer = this;
+        this.difficulty = 2; //from radio-button
+        this.game = [];
+        for (let i = 0; i < this.difficulty; i++) {
+            this.game.push(this.difficulty + i);
         }
-        return input;
+        this.game.shuffle();
+        for (let i = 0; i < this.difficulty; i++) {
+            this.game[this.game[i]] = i;
+        }
+        this.croupier(this.difficulty*2);
+        this.rotateNumer = 0;
+        this.rotateCard = -1;
+        this.matches = 0;
+
+        let table = document.getElementById('table');
+        table.addEventListener('click', function startTimer(event) {
+            table.removeEventListener('click', startTimer);
+            gamer.gameStartTimer();
+        });
+
+        table.addEventListener('click', function rot(event) {
+            console.log(event.target.id);
+            if (event.target.getAttribute('class') == 'card') gamer.rotate(event.target.id);
+        });
+    }
+    gameStartTimer() {
+        console.log('startTimer');
+    }
+    croupier(n) {//add card on table
+        let table = document.getElementById('table');
+        table.innerHTML = '';
+        for (let i = 0; i < n; i++) {
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.setAttribute('tabindex', i + 1);
+            //card.setAttribute('onclick', 'function(){rot('+i+');}');
+            card.id = i;
+            card.innerHTML = '';
+            table.appendChild(card);
+        };
+    };
+
+    unrotate() {
+        //style-unrotate
+        //for all with attribute rotate
     }
 
-    //all work only within #table
-    // id -in order
-    // pair - from shuffle
-    let difficulty = 8;
-    let n = difficulty * 2;
-    let table = document.getElementById('table');
+    rotate(id) {
 
-    for (let i = 0; i < n; i++) {
-        createCard(i, table);
+        console.log('id=' + id);
+
+        let card = document.getElementById(id);
+        //style-rotate
+        if (!this.rotateNumer) {
+            //start timer
+        }
+        this.rotateNumer++;
+        //only one card rotate
+        if (this.rotateCard == -1) {
+            this.rotateCard = id;
+            return true;
+        }
+        //two cards rotate
+        if (this.rotateCard == this.game[id]) {
+            this.matches += 2;
+            //hide cards
+            if (this.matches == this.game.length) {
+                //win
+                alert('win');
+            }
+        } else {
+            this.unrotate();
+        }
+
+        this.rotateCard = -1;
+
     }
-    function createCard(id, container) {
-        const card = document.createElement('div');
-        card.className = 'card';
-        card.setAttribute('tabindex', 100 + id);
-        card.id = id;
-        card.innerHTML = '';
-        container.appendChild(card);
-    }
 
-};
+}
 
 
-
-
-
-
-alert('document ready? really?');
-Game();
+var g = null;
+function start() {
+    g = new Game;
+}
